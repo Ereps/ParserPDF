bc5660fb271dc4bc1e7a8cc67e4801102fb98f6f
Ereps <erep23@hotmail.com>
2024-01-29 14:15:19 +0100

    test avec python

---
+++ testPython.py
@@ -0,0 +1,19 @@
+import pdftotext
+
+# Load your PDF
+with open("Corpus_test/Boudin-Torres-2006.pdf", "rb") as f:
+    pdf = pdftotext.PDF(f)
+
+# How many pages?
+print(len(pdf))
+
+# Iterate over all the pages
+for page in pdf:
+    print(page)
+
+# Read some individual pages
+print(pdf[0])
+print(pdf[1])
+
+# Read all the text into one string
+print("\n\n".join(pdf))

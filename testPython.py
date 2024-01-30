c579d83cda4865717adf45b18b58aecde7741277
Ereps <erep23@hotmail.com>
2024-01-30 13:20:32 +0100

    PyPdf2 lit toutes les pages

--- testPython.py
+++ testPython.py
@@ -3,12 +3,9 @@ from PyPDF2 import PdfReader
 #create file object variable
 #opening method will be rb
 reader=PdfReader("Corpus_test/Boudin-Torres-2006.pdf",'rb')
-numPages = reader.numPages
-
-for(i in range(0,numPages)):
-    
-page = reader.pages[0]
-
-text = page.extract_text()
+numPages = len(reader.pages)
 file1=open("Corpus_result/Boudin-Torres-2006.txt","a")
-file1.writelines(text)
+for i in range(0,numPages):
+    page = reader.pages[i]
+    text = page.extract_text()
+    file1.writelines(text)

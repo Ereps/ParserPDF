ff338cdcb89920c4436f887cf13ac0270562b70d
Lordonzo <vthevenon29@outlook.fr>
2024-02-13 12:48:57 +0100

    firstMethod with Metadata

--- firstMehod.py
+++ firstMehod.py
@@ -2,9 +2,11 @@ import directory, fitz
 
 # First method with metadata fields
 for pdf in directory.readfiles(directory.input_name) :
-    outputName = "./" + directory.output_name + pdf + ".txt"
-    print(outputName)
+    outputName = directory.output_name + pdf + ".txt"
+    #print(outputName)
     with fitz.open(pdf) as doc :
         if (doc.metadata.get("title") != "") :
-            with open(outputName, "w", encoding="utf-8") as output :
-                output.write("Title: " + doc.metadata.get("title"))
+            with open("../" + outputName, 'w+', encoding="utf-8") as output :
+                print(doc.metadata)
+                output.write("Title: " + doc.metadata.get("title") + "\n")
+                output.write("Author: " + doc.metadata.get("author"))

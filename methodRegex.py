925083fcc879e441d7f98385ef801c10ea861188
Lordonzo <vthevenon29@outlook.fr>
2024-02-18 12:50:15 +0100

    test for names

--- methodRegex.py
+++ methodRegex.py
@@ -1,4 +1,7 @@
 import re, directory, fitz
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 # Method with Regex
 for pdf in directory.readfiles(directory.input_name) :
@@ -8,8 +11,21 @@ for pdf in directory.readfiles(directory.input_name) :
         print(pdf)
         page = doc.load_page(0)
         blocks = page.get_textpage().extractBLOCKS()
+        text = page.get_textpage().extractText()
         for block in blocks :
             name_regex = re.compile(r'[A-Z][a-z]*\.?\s+(?:[A-Z][a-z]*\.?\s*)?[A-Z][a-z]+$', re.MULTILINE)
             name_regex = re.compile(r'[A-Z][a-z]*\.? +(?:[A-Z][a-z]*\.? *)*[A-Z][a-z]+$', re.MULTILINE)
             print(name_regex.findall(block[4]))
             #print(re.findall(r"^Abstract", block[4]))
+
+
+# j'ai essayé d'utiliser une librairie pour les noms mais ça fonctionne qu'à moitié...
+def extract_person_names(text):
+    doc = nlp(text)
+    persons = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
+    return persons
+
+person_names = extract_person_names(text)
+print("Noms propres de personnes trouvés dans le PDF :")
+for name in person_names:
+    print(name)

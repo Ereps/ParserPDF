d18fcb5fb48be268cbdde6afd8646ef08c7ec5fc
MANUE <enmaling@gmail.com>
2024-02-20 13:40:11 +0100

    accents + commentaires titre

--- parserSprint3.py
+++ parserSprint3.py
@@ -91,34 +91,35 @@ def extract_abstract(text):
     # If either introduction keyword is not found, return empty string
     return ""
 
+'''récupération du titre du pdf'''
 def extract_title(outputFname, doc):
     title = ""
-    if doc.metadata.get("title") != "":
+    if doc.metadata.get("title") != "": #si le titre apparaît dans la metadata
         txt = ""
         line = " "
         i = 1
-        if "<" in lc.getline(outputFname, i):
+        if "<" in lc.getline(outputFname, i): #si le premier paragraphe commence par un "<"
             i += 1
-        while line != "":
-            line = lc.getline(outputFname, i).rstrip("\n")
+        while line != "": #tant qu'on est pas à la fin du paragraphe
+            line = lc.getline(outputFname, i).rstrip("\n") #on récupère la ligne et on enlève le caractère de fin de ligne
             txt += line + " "
             i += 1
-        if txt.rstrip(" ") == doc.metadata.get("title"):
+        if txt.rstrip(" ") == doc.metadata.get("title"): #si le titre est égal au paragraphe récupéré
             title = txt
-        else:
+        else: #sinon
             txt = " "
             line = " "
-            while line != "":
-                line = lc.getline(outputFname, i).rstrip("\n")
+            while line != "": #tant qu'on est pas à la fin du paragraphe
+                line = lc.getline(outputFname, i).rstrip("\n") #on récupère la ligne et on enlève le caractère de fin de ligne
                 txt += line + " "
                 i += 1
-            title = txt.rstrip(" ")
-    else:
+            title = txt.rstrip(" ") #on enlève le dernier espace du titre
+    else: #sinon
         txt = ""
         line = " "
         i = 1
-        while line != "":
-            line = lc.getline(outputFname, i).rstrip("\n")
+        while line != "": #tant qu'on est pas à la fin du paragraphe
+            line = lc.getline(outputFname, i).rstrip("\n") #on récupère la ligne et on enlève le caractère de fin de ligne
             txt += line + " "
             i += 1
         title = txt
@@ -153,7 +154,6 @@ def extract_authors(outputFname, title):
             author_string += line
             line = file.readline()
             if re.search(r'Abstract|In this article|This article', line):
-                #TODO Comme titre, trouver les cinqs premiers mots
                 break
     
 
@@ -161,9 +161,72 @@ def extract_authors(outputFname, title):
     author_string = author_string.strip()
     author_string = author_string.replace('\n', ' ')
     author_string = author_string.replace('- ', '')
+    author_string = author_string.replace('`A', 'À')
+    author_string = author_string.replace('^A', 'Â')
+    author_string = author_string.replace('"A', 'Ä')
+    author_string = author_string.replace('`E', 'È')
+    author_string = author_string.replace('¨E', 'Ë')
+    author_string = author_string.replace('^E', 'Ê')
+    author_string = author_string.replace('¨I', 'Ï')
+    author_string = author_string.replace('^I', 'Î')
+    author_string = author_string.replace('`I', 'Ì')
+    author_string = author_string.replace('`U', 'Ù')
+    author_string = author_string.replace('¨U', 'Ü')
+    author_string = author_string.replace('`¨U', 'Ǜ')
+    author_string = author_string.replace('^U', 'Û')
+    author_string = author_string.replace('`O', 'Ò')
+    author_string = author_string.replace('¨O', 'Ö')
+    author_string = author_string.replace('^O', 'Ô')
+    author_string = author_string.replace('`Y', 'Ỳ')
+    author_string = author_string.replace('¨Y', 'Ÿ')
+    author_string = author_string.replace('^Y', 'Ŷ')
+    author_string = author_string.replace('^Z', 'Ẑ')
+    author_string = author_string.replace('^S', 'Ŝ')
+    author_string = author_string.replace('^G', 'Ĝ')
+    author_string = author_string.replace('^H', 'Ĥ')
+    author_string = author_string.replace('¨H', 'Ḧ')
+    author_string = author_string.replace('^J', 'Ĵ')
+    author_string = author_string.replace('¨W', 'Ẅ')
+    author_string = author_string.replace('^W', 'Ŵ')
+    author_string = author_string.replace('`W', 'Ẁ')
+    author_string = author_string.replace('^C', 'Ĉ')
+    author_string = author_string.replace('¨X', 'Ẍ')
+    author_string = author_string.replace('`N', 'Ǹ')
     author_string = author_string.replace('´e', 'é')
     author_string = author_string.replace('`e', 'è')
-    author_string = author_string.replace('´a', 'à')
+    author_string = author_string.replace('^e', 'ê')
+    author_string = author_string.replace('"e', 'ë')
+    author_string = author_string.replace('"a', 'ä')
+    author_string = author_string.replace('^a', 'â')
+    author_string = author_string.replace('`a', 'à')
+    author_string = author_string.replace('°a', 'å')
+    author_string = author_string.replace('"i', 'ï')
+    author_string = author_string.replace('^i', 'î')
+    author_string = author_string.replace('`i', 'ì')
+    author_string = author_string.replace('`u', 'ù')
+    author_string = author_string.replace('`¨u', 'ǜ')
+    author_string = author_string.replace('"u', 'ü')
+    author_string = author_string.replace('^u', 'û')
+    author_string = author_string.replace('`o', 'ò')
+    author_string = author_string.replace('"o', 'ö')
+    author_string = author_string.replace('^o', 'ô')
+    author_string = author_string.replace('`y', 'ỳ')
+    author_string = author_string.replace('"y', 'ÿ')
+    author_string = author_string.replace('^y', 'ŷ')
+    author_string = author_string.replace('´y', 'ý')
+    author_string = author_string.replace('^z', 'ẑ')
+    author_string = author_string.replace('^s', 'ŝ')
+    author_string = author_string.replace('^g', 'ĝ')
+    author_string = author_string.replace('^h', 'ĥ')
+    author_string = author_string.replace('"h', 'ḧ')
+    author_string = author_string.replace('^j', 'ĵ')
+    author_string = author_string.replace('`w', 'ẁ')
+    author_string = author_string.replace('^w', 'ŵ')
+    author_string = author_string.replace('"w', 'ẅ')
+    author_string = author_string.replace('^c', 'ĉ')
+    author_string = author_string.replace('"t', 'ẗ')
+    author_string = author_string.replace('"x', 'ẍ')
+    author_string = author_string.replace('`n', 'ǹ')
 
     # Return the extracted author information
     return author_string

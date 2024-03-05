43059acb21ce63d62e69616cecd0346dd813de5c
Ereps <erep23@hotmail.com>
2024-03-05 15:29:49 +0100

    My bad scrum master j'avais pas pull du coup j'ai suppr le intro.py

--- extract/introduction.py
+++ extract/introduction.py
@@ -22,3 +22,15 @@ def extract(blocks):
                 intro_index = i
                 break
     return intro_index
+
+"""
+notes:
+l'intro peut être constituée de un ou plusieurs blocks
+il y a toujours le mot clé "Introduction" ou "INTRODUCTION"
+les annotations de bas de page (considérées comme un block à part) sont à gérer
+si l'introduction est sur plusieurs pages, le haut de page est à gérer
+cas: Mikolov.pdf on a [1.1 Goals of the paper et 1.2 Previous work] des sous parties de Introduction ? demander à Mr. Kessler si c'est à mettre dans la partie Introduction ou Corps
+idée délimiter partie Introduction : la partie Introduction est tout le temps suivie par 2 | 2. | II. {titre de la partie}, avec une police d'écriture plus grosse que le texte normal (ou en bold parfois)
+idée annotations : virer les blocs de taille inférieur à X caractères ?
+
+"""

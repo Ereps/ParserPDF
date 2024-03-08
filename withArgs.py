3dac81c1942f6566fbc4d7851a9a953ea1d65755
Lordonzo <vthevenon29@outlook.fr>
2024-03-08 18:31:07 +0100

    arg update + todo: files

--- withArgs.py
+++ withArgs.py
@@ -1,4 +1,4 @@
-import sys, getopt, time, os, argparse
+import sys, getopt, time, os, argparse, glob
 
 def helpUsage() :
     print(os.path.basename(__file__), '[options]')
@@ -8,14 +8,14 @@ def helpUsage() :
     print('-a | --all   : exports the PDF file to XML and Text')
 
 # TODO: to XML
-def toXml() :
+def toXml(files : list) :
     startTime = time.time()
     print('exporting to xml...')
     #toXML.buildXML()
     print('--- XML in %s seconds ---' % (time.time() - startTime))
     
 # TODO: to Text
-def toText() :
+def toText(files : list) :
     startTime = time.time()
     print('exporting to text...')
     
@@ -26,13 +26,10 @@ def main() :
     parser = argparse.ArgumentParser(prog=os.path.basename(__file__), usage='%(prog)s [options]')
     parser.add_argument('-x', '--xml', help='export the PDF file to XML', action='store_true')
     parser.add_argument('-t', '--text', help='export the PDF file to text', action='store_true')
-    parser.add_argument('-i', '--input', help='input, use with --dir or --file', action='store_true')
-    # TODO: use sub_parser I think
-    parser.add_argument('--dir', help='directory with PDF files to convert', action='store', type=str, nargs=1, metavar=('folder'))
-    parser.add_argument('--file', help='PDF file(s) to convert', action='append', type=str, nargs='+', metavar=('file'))
+    parser.add_argument('-d', '--dir', help='directory with PDF files to convert', action='store', type=str, nargs=1, metavar=('folder'))
+    #TODO: parser.add_argument('-f', '--file', help='PDF file(s) to convert', action='append', type=str, nargs='+', metavar=('file'))
     args = vars(parser.parse_args())
-    print(args) # https://stackoverflow.com/questions/51495070/python-argparse-subparsers
-    
+    #print(args) # https://stackoverflow.com/questions/51495070/python-argparse-subparsers
     '''
     nargs stands for Number Of Arguments
 
@@ -42,29 +39,36 @@ def main() :
         +: like *, but requiring at least one value
         argparse.REMAINDER: all the values that are remaining in the command line
     '''
-    '''
-    try :
-        opts, args = getopt.getopt(argv, 'hxtad', ['help', 'xml', 'text', 'all'])
-    except getopt.GetoptError as err :
-        print(err)
-        sys.exit(2)
-    for o, a in opts :
-        if o in ('-h', '--help') :
-            helpUsage()
-            sys.exit()
-        elif o in ('-x', '--xml') :
-            toXml()
-        elif o in ('-t', '--text') :
-            toText()
-        elif o in ('-a', '--all') :
-            toText()
-            toXml()
-        elif o == '-d' :
-            global debug
-            debug = 1
+    if not args['xml'] and not args['text'] :
+        print('Need at least one export to select : --help to see what you can do')
+        exit(1)
+    if os.path.isdir(args['dir'][0]) :
+        files = [os.path.join(args['dir'][0], f) for f in os.listdir(args['dir'][0]) if os.path.isfile(os.path.join(args['dir'][0], f)) and f.endswith('.pdf')]
+        if not files :
+            print('No PDF file in this directory')
+            exit(1)
         else :
-            assert False, "unhandled option"
-    '''
+            hashmap = {}
+            for i in range(len(files)) :
+                hashmap[i] = files[i]
+            # Print the files to select
+            print('Please select which of those files you want to make the export :')
+            print('%2s' % '*', '->', 'Select all')
+            for i in range(len(hashmap)) :
+                print('%2s' % i, '->', hashmap.get(i))
+            selection = input('Entry (separator is the space): ')
+            selection = list(selection.split(' '))
+            final_selection = []
+            if '*' in selection :
+                final_selection = files
+            else :
+                for select_index in selection :
+                    final_selection.append(hashmap.get(int(select_index)))
+            #print(final_selection)
+            if args['xml'] : 
+                toXml(final_selection)
+            if args['text'] :
+                toText(final_selection)
 
 if __name__ == '__main__' :
     main()

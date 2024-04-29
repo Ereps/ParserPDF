65b6041179ce09f4ed5fc8502414976824c0cbb9
Virgile <vthevenon29@outlook.fr>
2024-04-29 13:31:54 +0200

    title patch SURVEY

--- extract/title.py
+++ extract/title.py
@@ -1,31 +1,25 @@
+import re
 from extract.block_treatement import *
 
 """récupération du titre du pdf"""
 #TODO extraction du titre avec la taille de police plutot qu'a la louche
 def extract(blocks: list, doc: open) -> str :
     title = ""
-    i = 0
-    title_block = 0
-    if doc.metadata.get("title") != "": #si le titre apparaît dans la metadata
+    if doc.metadata.get("title") != "" : #si le titre apparaît dans la metadata
+        #print('BRGH')
+        pattern = re.compile(r"(%s).*" % doc.metadata.get('title'))
+        #print(doc.metadata.get("title"))
         txt = ""
-        block_text = replace_special_char(blocks[i][4])
-        if "<" in block_text: #si le premier paragraphe commence par un "<"
-            i += 1
-        title_block = i
-        txt = replace_special_char(blocks[i][4])
-        i += 1
-        if txt.rstrip(" ") == doc.metadata.get("title"): #si le titre est égal au paragraphe récupéré
-            title = txt
-        else: #sinon
-            title_block = i
-            txt = replace_special_char(blocks[i][4]) #on récupère la ligne et on enlève le caractère de fin de ligne
-            title = txt.rstrip(" ") #on enlève le dernier espace du titre
+        for i in range(len(blocks)) :
+            block_text = replace_special_char(blocks[i][4])
+            if pattern.match(block_text) :
+                return block_text
     else: #si le titre n'est pas dans les metadatas
         #TODO trouver une solution avec la police d'écriture
-        title_block = i
-        txt = replace_special_char(blocks[i][4]) #on récupère la ligne et on enlève le caractère de fin de ligne
+        txt = replace_special_char(blocks[0][4]) #on récupère la ligne et on enlève le caractère de fin de ligne
         title = txt
 
     title = title.replace('\n', '')
     title = title.strip()
+    #print('Title ', i)
     return title

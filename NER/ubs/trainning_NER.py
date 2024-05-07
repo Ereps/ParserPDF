539790d46b5a94f903686b3bd731a8ba92eebbfb
Ereps <erep23@hotmail.com>
2024-05-07 14:13:11 +0200

    test authors ner

--- NER/ubs/trainning_NER.py
+++ NER/ubs/trainning_NER.py
@@ -1,10 +1,7 @@
 import json
 import spacy
-import en_core_web_sm
 import re
-import random
 import making_dataset
-
 def test_model(text):
     """
     Test the NER model on the given text and extract entities labeled as "AUTHOR".
@@ -15,6 +12,7 @@ def test_model(text):
     Returns:
         list: A list of extracted entities labeled as "AUTHOR".
     """
+    nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
     results = []
     for ent in doc.ents:
@@ -25,10 +23,89 @@ def test_model(text):
     print(results)
     return results
 
+def replace_special_char(text):
+    text = text.strip()
+    continus_word = re.compile(r'-( )*\n')
+    text = re.sub(continus_word,"",text)
+    text = text.replace('\n', ' ')
+    text = text.replace('- ', '')
+    text = text.replace('`A', 'À')
+    text = text.replace('^A', 'Â')
+    text = text.replace('"A', 'Ä')
+    text = text.replace('`E', 'È')
+    text = text.replace('¨E', 'Ë')
+    text = text.replace('^E', 'Ê')
+    text = text.replace('´E', 'É')
+    text = text.replace('¨I', 'Ï')
+    text = text.replace('^I', 'Î')
+    text = text.replace('`I', 'Ì')
+    text = text.replace('`U', 'Ù')
+    text = text.replace('¨U', 'Ü')
+    text = text.replace('`¨U', 'Ǜ')
+    text = text.replace('^U', 'Û')
+    text = text.replace('`O', 'Ò')
+    text = text.replace('¨O', 'Ö')
+    text = text.replace('^O', 'Ô')
+    text = text.replace('`Y', 'Ỳ')
+    text = text.replace('¨Y', 'Ÿ')
+    text = text.replace('^Y', 'Ŷ')
+    text = text.replace('^Z', 'Ẑ')
+    text = text.replace('^S', 'Ŝ')
+    text = text.replace('^G', 'Ĝ')
+    text = text.replace('^H', 'Ĥ')
+    text = text.replace('¨H', 'Ḧ')
+    text = text.replace('^J', 'Ĵ')
+    text = text.replace('¨W', 'Ẅ')
+    text = text.replace('^W', 'Ŵ')
+    text = text.replace('`W', 'Ẁ')
+    text = text.replace('^C', 'Ĉ')
+    text = text.replace('¨X', 'Ẍ')
+    text = text.replace('`N', 'Ǹ')
+    text = text.replace('´e', 'é')
+    text = text.replace('`e', 'è')
+    text = text.replace('^e', 'ê')
+    text = text.replace('"e', 'ë')
+    text = text.replace('¨a', 'ä')
+    text = text.replace('^a', 'â')
+    text = text.replace('`a', 'à')
+    text = text.replace('´a', 'á')
+    text = text.replace('°a', 'å')
+    text = text.replace('"ı', 'ï')
+    text = text.replace('ˆı', 'î')
+    text = text.replace('`ı', 'ì')
+    text = text.replace('`u', 'ù')
+    text = text.replace('`¨u', 'ǜ')
+    text = text.replace('"u', 'ü')
+    text = text.replace('^u', 'û')
+    text = text.replace('`o', 'ò')
+    text = text.replace('"o', 'ö')
+    text = text.replace('^o', 'ô')
+    text = text.replace('`y', 'ỳ')
+    text = text.replace('"y', 'ÿ')
+    text = text.replace('^y', 'ŷ')
+    text = text.replace('´y', 'ý')
+    text = text.replace('^z', 'ẑ')
+    text = text.replace('^s', 'ŝ')
+    text = text.replace('^g', 'ĝ')
+    text = text.replace('^h', 'ĥ')
+    text = text.replace('"h', 'ḧ')
+    text = text.replace('^j', 'ĵ')
+    text = text.replace('`w', 'ẁ')
+    text = text.replace('^w', 'ŵ')
+    text = text.replace('"w', 'ẅ')
+    text = text.replace('^c', 'ĉ')
+    text = text.replace('c¸', 'ç')
+    text = text.replace('"t', 'ẗ')
+    text = text.replace('"x', 'ẍ')
+    text = text.replace('`n', 'ǹ')
+
+    return text
+
 def process_author(text :str) ->str:
     noNum = r'[0-9]+'
     final_text = re.sub(noNum,"",text)
-    accent = "éèêëäâàáåïîìùǜüûòöôỳÿŷýẑŝĝĥḧĵẁŵẅĉçẗẍǹ"
+    final_text = replace_special_char(final_text)
+    accent = "éèêëäâàáåïîìùǜüûòöôỳÿŷýẑŝĝĥḧĵẁŵẅĉçẗẍǹÉÈÊËÄÂÀÁÅÏÎÌÙǛÜÛÒÖÔỲŸŶÝẐŜĜĤḦĴẀŴẄĈÇT̈ẌǸ"
     #Remove non letter character
     final_text = re.sub(r'[^a-zA-Z\s' +accent+r']', '', final_text)
     #Remove space
@@ -51,7 +128,7 @@ def generate_rules(file):
     Returns:
         None
     """
-    nlp = en_core_web_sm.load()
+    nlp = spacy.load("en_core_web_sm")
     ruler = nlp.add_pipe("entity_ruler")
     i = 0
     with open(file, "r") as file:
@@ -75,7 +152,7 @@ if __name__ == "__main__":
 
 
     #__TEST NER
-    nlp = en_core_web_sm.load()
+    #nlp = spacy.load("xx_sent_ud_sm")
 
     for file_name in txt_list:
         with open(txt_dir + file_name, "r") as file:
---
+++ extract/authors_emails.py
@@ -0,0 +1,268 @@
+import re
+
+from extract.block_treatement import *
+
+
+import en_core_web_sm
+
+def extract_ner(text) ->list:
+    nlp = en_core_web_sm.load()
+    doc = nlp(text)
+    results = []
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            author = process_author(ent.text)
+            if(author !=""):
+                results.append(author)
+    print(results)
+    return results
+
+def process_author(text :str) ->str:
+    noNum = r'[0-9]+'
+    final_text = re.sub(noNum,"",text)
+    final_text = replace_special_char(final_text)
+    accent = "éèêëäâàáåïîìùǜüûòöôỳÿŷýẑŝĝĥḧĵẁŵẅĉçẗẍǹÉÈÊËÄÂÀÁÅÏÎÌÙǛÜÛÒÖÔỲŸŶÝẐŜĜĤḦĴẀŴẄĈÇT̈ẌǸ"
+    #Remove non letter character
+    final_text = re.sub(r'[^a-zA-Z\s' +accent+r']', '', final_text)
+    #Remove space
+    final_text = final_text.strip()
+    #Remove if the text is 2 or less character
+    if(len(final_text) < 3):
+        final_text = ""
+    #Remove if the first character isn't in uppercase
+    elif(final_text[0].islower()):
+        final_text = ""
+    
+    return final_text
+    
+    
+
+
+
+
+def extract(blocks, index, abstract_index) -> list:
+    email = []
+    author = []
+    emails = []
+    authors = []
+    author_email=[]
+    affiliation = {}
+    a = []
+    e = []
+    date = []
+    no_no_in = False
+    affil_in = False
+    affil = ''
+    cpt = 1
+    author_pattern = re.compile(r'[A-Z][a-zàáâäçèéêëìíîïñòóôöùúûüýÿﬁ]+(?:-[A-Za-zàáâäçèéêëìíîïñòóôöùúûüýÿﬁ]*)?(?: +[A-Zinhugdlaeiouàáâäçèéêëìíîïñòóôöùúûüýÿﬁ.]*)?(?:[.]*)? [A-Z][A-Za-zàáâäçèéêëìíîïñòóôöùúûüýÿﬁ]+(?:-[A-Za-zàáâäçèéêëìíîïñòóôöùúûüýÿﬁ-]*)?')
+    email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+[@qQ][A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+    semi_mail_pattern = re.compile(r'[@qQ][A-Za-z0-9.-]+\.[A-Z|a-z]{2,}')
+    date_pattern = re.compile(r'(?:(?:[January]{7})|(?:[February]{8})|(?:[March]{5})|(?:[April]{5})|(?:[May]{3})|(?:[June]{4})|(?:[July]{4})|(?:[August]{6})|(?:[September]{9})|(?:[October]{7})|(?:[November]{8})|(?:[December]{8}))[ ]*[0-9]{1,4}[, ]*[0-9]{1,4}')
+    no_no_words = ['Université', 'Bretagne', 'Nord', 'Sud', 'Est', 'Ouest', 'University', 'Universitat', 'North', 'South', 'West', 'Laboratoire', 'Laboratory', 'Rennes', 'Informatique', 'Centre', 'Center', 'Europe', 'Google', 'Inc', 'Fondamentale', 'Marseille', 'France', 'Aix-Marseille', 'Vannes', 'Canada', 'Montréal', 'Polytechnique', 'Mexico', 'Avignon', 'Instituto', 'Ingeniería', 'Institute', 'Institue', 'Linguistics', 'Spain', 'Mexique', 'Espagne', 'Québec', 'Pays', 'Vaucluse', 'Meinajaries', 'Département', 'Centre-ville', 'New York', 'Department', 'Computer', 'Science', 'Columbia', 'Technologies', 'Carnegie', 'Mountain', 'View', 'Ecole', 'Centre', 'Ville', 'Cedex', 'Scalable', 'Approach', 'Sentence', 'Scoring', 'Multi', 'Document', 'Multi-Document', 'Update', 'Word', 'Representations', 'Vector', 'Space', 'System', 'Demonstrations', 'Processing', 'Tool', 'Matière', 'Condensée', 'Compiled', 'April', 'November', 'January', 'February', 'March', 'May', 'June', 'July', 'August', 'September', 'December', 'Institut', 'Universitari', 'Lingüística', 'Aplicada', 'Barcelona', 'La', 'Rambla', 'Xerox', 'Research', 'Artiﬁcial', 'Arificial', 'Intelligence']
+    if index == 0:
+        index += 1
+    # Trouver l'indice du bloc contenant le titre
+    print(index)
+    print(abstract_index)
+    #abstract_index[0] == premier abstract block
+    for i in range(index, abstract_index, 1):
+        if 'Abstract' in blocks[i][4] or 'abstract' in blocks[i][4] or 'ABSTRACT' in blocks[i][4]:
+            i += 1
+            if abstract_index <= i:
+                break
+        block_text = replace_special_char(blocks[i][4]) #remplace tous les accents
+        author_match = author_pattern.search(block_text) #cherche les auteurs
+        email_match = email_pattern.search(block_text) #cherche les mails
+        semi_mail_match = semi_mail_pattern.search(block_text) #cherche les fins de mails
+        date_match = date_pattern.search(block_text)#cherche des dates
+        if(date_match):
+            date.append(date_pattern.findall(block_text))
+            for d in date[0]:
+                block_text = block_text.replace(d, '')#supprime les dates
+
+        if 'J. Manuel Torres Moreno' in block_text:
+            a.append(['J. Manuel Torres Moreno'])
+        block_text = block_text.replace('J. Manuel Torres Moreno', '')
+        #print(block_text)
+        if(author_match): #si on a trouvé des auteurs
+            a.append(author_pattern.findall(block_text)) #ajoute dans la liste auteurs
+        print(block_text)
+        for w in a:
+            for x in w:
+                author.append(x)
+        """
+        TEST NER
+        a = []
+
+        #print(author)
+        for y in range(len(author)): #boucle sur la liste auteur
+            for z in no_no_words: #boucle sur la liste des mots non voulu
+                if z in author[y]: #si un mot non voulu est trouvé dans la string
+                    no_no_in = True #on passe no_no_in a true
+            if no_no_in == False: #si no_no_in est false
+                authors.append(author[y]) #on l'ajoute à la liste définitive des auteurs
+            no_no_in = False #on remet no_no_in a false
+        author = []
+        #print('après no_no_word')
+        #print(authors)
+        
+        #print(block_text)
+        for t in authors:
+            #print(t)
+            block_text = block_text.replace(t, '') #supprime les auteurs du texte
+            #print(block_text)
+        """
+        a = extract_ner(block_text)
+        print(authors)
+        if(email_match): #si on a trouvé des mails
+            email.append(email_pattern.findall(block_text)) #ajoute dans la liste de mails
+            emails = [element for sous_liste in email for element in sous_liste]
+            if len(email) == 1: #si on a qu'un seul mail
+                email_match = email_pattern.search(block_text)
+                email_index = email_match.start()
+                if block_text[email_index-1] == ',' or block_text[email_index-2] == ',': #si jamais on trouve une virgule avant le mail
+                    #block_text = block_text.replace(' ', '')
+                    email_match = email_pattern.search(block_text)
+                    email_index = email_match.start()
+                    semi_mail_match = semi_mail_pattern.search(block_text)
+                    end_email = semi_mail_pattern.findall(block_text)
+                    e = block_text.split(',') #on sépare le texte grâce aux virgules
+                    for m in e: #on boucle sur les éléments du texte séparé
+                        if m != email[0][0] and ' ' not in m and m != '': #si l'élément est différent du mail
+                            emails.append(m+end_email[0]) #on l'ajoute à la liste des mails
+        elif(semi_mail_match): # sinon si on a trouvé une fin de mail
+            block_text_no_space = block_text.replace(' ', '') #on enlève tous les espaces
+            semi_mail_match = semi_mail_pattern.search(block_text_no_space)
+            semi_mail_index = semi_mail_match.start() #on cherche où la fin du mail commence
+            end_email = semi_mail_pattern.findall(block_text_no_space) #on récupère la fin du mail
+            mails = ""
+            mails_space = ''
+            if block_text_no_space[(semi_mail_index-1)] == ')':#si l'ensemble des débuts de mails est contenu entre parenthèse
+                y = semi_mail_index-2
+                y_space = y
+                while block_text_no_space[y] != '(':#on boucle jusqu'à ce qu'on trouve la parenthèse fermante
+                    mails = block_text_no_space[y] + mails #on récupère le texte dans entre parenthèse
+                    y -= 1
+                mail_sep = mails.split(',')
+                mails_space = '('
+                for mail in mail_sep:
+                    emails.append(mail+end_email[0])
+                    mails_space += mail + ', '
+                mails_space = mails_space[:-2] + ')'
+                block_text = block_text.replace(mails_space, '')
+            elif block_text_no_space[(semi_mail_index-1)] == '}':#sinon si l'ensemble des débuts de mails est contenu entre chevrons
+                y = semi_mail_index-2
+                y_space = y
+                while block_text_no_space[y] != '{': #on boucle jusqu'à ce qu'on trouve le chevron fermant
+                    mails = block_text_no_space[y] + mails #on récupère le texte entre chevrons
+                    y -= 1
+                mail_sep = mails.split(',')
+                mails_space = '{'
+                for mail in mail_sep:
+                    emails.append(mail+end_email[0])
+                    mails_space += mail + ', '
+                mails_space = mails_space[:-2] + '}'
+                block_text = block_text.replace(mails_space, '')
+            elif block_text_no_space[(semi_mail_index-1)] == ']':#sinon si l'ensemble des débuts de mails est contenu entre crochets
+                y = semi_mail_index-2
+                y_space = y
+                while block_text_no_space[y] != '[':#on boucle jusqu'à ce qu'on trouve le crochet fermant
+                    mails = block_text_no_space[y] + mails #on récupère le texte entre crochets
+                    y -= 1
+                mail_sep = mails.split(',') #on sépare le texte grâce aux virgules
+                mails_space = '['
+                for mail in mail_sep:#boucle sur chaques débuts de mails
+                    emails.append(mail+end_email[0])#on ajoute le début de mail et la fin à la liste email
+                    mails_space += mail + ', '
+                mails_space = mails_space[:-2] + ']'
+                block_text = block_text.replace(mails_space, '')
+            block_text = block_text.replace(end_email[0], '')
+        for z in emails:
+            block_text = block_text.replace(z, '') #supprime les mails du texte
+        notWanted = re.compile(r' *[&♮♭*∗†]+ *|[, ]+$|^[ ]+| *[0-9,sthrnd]{3}(?:[()A-Za-z]{3})?(?: +|$)|(?:[(][a-z-,.]*[)])')
+        nw = notWanted.findall(block_text)
+        for n in nw:
+            block_text = block_text.replace(n, ' ')
+        notAffiliation = re.compile(r' *[,*.]+ *[,*. ]*| *[and]{3} *| +$')
+        isAffiliation = notAffiliation.fullmatch(block_text)
+        if block_text == '':
+            isAffiliation = ''
+        if isAffiliation == None:
+            if block_text.endswith(' '):
+                print('suffix')
+                block_text = block_text.removesuffix(' ')
+            if block_text.startswith(' '):
+                print('prefix')
+                block_text = block_text.removeprefix(' ')
+            ajout = False
+            same = True
+            for r in authors:
+                if r not in affiliation:
+                    affiliation[r] = block_text.strip()
+                    affil_in = True
+                    ajout = True
+            if ajout == False and cpt < 3:
+                affiliations = {}
+                v = affiliation[authors[0]]
+                for key, value in affiliation.items():
+                    if value != v:
+                        same = False
+                if same:
+                    for key, value in affiliation.items():
+                        affiliations[key] = value + ' ' + block_text.strip()
+                    affiliation = affiliations
+                    cpt += 1
+
+
+    print(author)
+    print(emails)
+    print(affiliation)
+        
+
+    for em in emails:
+        ema = ''
+        for c in em:
+            if c == '@' or c == 'Q':
+                break
+            else:
+                ema += c
+        cpta = 0
+        cpt = 0
+        auth=''
+        sema = ema
+        point = re.compile('^[.]+$')
+        for aut in authors:
+            for au in aut:
+                sau = suppr_special_char(au).lower()
+                ema = sema
+                onlypoint = point.search(ema)
+                if sau != '' and ema != '' and not onlypoint:
+                    if sau in ema:
+                        cpt += 1
+                        iema = sema.find(sau)
+                        lema = list(sema)
+                        lema.pop(iema)
+                        sema = ''.join(lema)
+            if cpt > cpta:
+                auth = aut
+            cpt = 0
+        if affil_in == True:
+            affil = affiliation[auth]
+        author_email.append([auth, em, affil])
+    
+    
+    if not(emails):
+        for d in authors:
+            if affil_in == True:
+                affil = affiliation[d]
+            author_email.append([d, 'N/A', affil])
+    
+    print(author_email)
+
+    for aut in author_email:
+        if aut[-1] == '':
+            aut[-1] = 'N/A'
+
+    print(author_email)
+
+    return author_email
